1bfb444bd91aa72cd852237df623fe66159d3b60
Retr0 <jessebrons17@gmail.com>
2023-07-26 13:10:52 +0200

    Update truncate_data.sh

--- truncate_data.sh
+++ truncate_data.sh
@@ -1,5 +1,5 @@
 CATEGORY="Business"
-ORIGIN_PATH=./dataset/
-TARGET_PATH=./dataset/truncated/
+ORIGIN_PATH="./dataset/"
+TARGET_PATH="./dataset/truncated/"
 
 python3 ./dataset/truncate_data.py $CATEGORY $ORIGIN_PATH $TARGET_PATH
