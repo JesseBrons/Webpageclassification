98fb1ffccf552c8230789610fe783e9e117dc269
Retr0 <jessebrons17@gmail.com>
2023-07-27 09:46:29 +0200

    Update train_model_BERT.sh

--- train_model_BERT.sh
+++ train_model_BERT.sh
@@ -1,6 +1,19 @@
+mixed_precision_list=(no fp16)
+model_list=(bert distilbert tinybert)
+
 NUMBER_GPU=$1
 MIXED_PREC=$2
 MODEL_NAME=$3
+
+if [[ ! " ${mixed_precision_list[*]} " =~ " ${MIXED_PREC} " ]]; then
+    echo "Invalid argument! Mixed precision ${MIXED_PREC} is not in (${mixed_precision_list[*]})."
+    exit
+fi
+
+if [[ ! " ${model_list[*]} " =~ " ${MODEL_NAME} " ]]; then
+    echo "Invalid argument! Model ${MODEL_NAME} is not in (${model_list[*]})."
+    exit
+fi
 
 MAIN_DIR=.
 
