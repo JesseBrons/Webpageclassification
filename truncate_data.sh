befcbde863b4109608c9b38a852484688729b9a6
Retr0 <jessebrons17@gmail.com>
2023-07-27 08:57:11 +0200

    Update truncate_data.sh

--- truncate_data.sh
+++ truncate_data.sh
@@ -1,5 +1,12 @@
-CATEGORY="Business"
+category_list=(arts business computers games home health news recreation reference science shopping society sports)
+
+CATEGORY=$1
 ORIGIN_PATH="./dataset/"
 TARGET_PATH="./dataset/truncated/"
+
+if [[ ! " ${category_list[*]} " =~ " ${CATEGORY} " ]]; then
+  echo "Invalid category! Category ${CATEGORY} is not in (${category_list[*]})."
+  exit
+fi
 
 python3 ./dataset/truncate_data.py $CATEGORY $ORIGIN_PATH $TARGET_PATH
