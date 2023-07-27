032f6a45da52c73c152d0b2eeb49c65aae161c77
Retr0 <jessebrons17@gmail.com>
2023-07-27 08:56:25 +0200

    Update crawl_websites.sh

--- crawl_websites.sh
+++ crawl_websites.sh
@@ -1,4 +1,11 @@
-CATEGORY="Business"
+category_list=(arts business computers games home health news recreation reference science shopping society sports)
+
+CATEGORY=$1
 DATA_FOLDER="./dataset/"
+
+if [[ ! " ${category_list[*]} " =~ " ${CATEGORY} " ]]; then
+  echo "Invalid category! Category ${CATEGORY} is not in (${category_list[*]})."
+  exit
+fi
 
 python3 ./dataset/crawl_websites.sh $CATEGORY $DATA_FOLDER
